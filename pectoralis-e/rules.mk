5a56fff38a350ca77651671334d822b4c2563a25
tnissy <mumei.f@yandex.com>
2024-01-29 13:55:59 +0900

    E2

--- pectoralis-e/rules.mk
+++ pectoralis-e/rules.mk
@@ -1,6 +1,5 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-LTO_ENABLE = yes
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 ORYX_ENABLE = yes
@@ -8,3 +7,4 @@ DYNAMIC_MACRO_ENABLE = yes
 SPACE_CADET_ENABLE = no
 COMBO_ENABLE = yes
 RGBLIGHT_ENABLE = no
+LTO_ENABLE = yes
